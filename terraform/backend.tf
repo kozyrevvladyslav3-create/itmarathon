2860862dc7fbfb0905e7a56ea50a89256b538199
kozyrevvladyslav3-create <kozyrevvladyslav3@gmail.com>
2025-10-30 21:22:09 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-marathon2025"
+    bucket       = "marathon2025"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
